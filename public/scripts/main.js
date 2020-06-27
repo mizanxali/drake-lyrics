aeea4bb21cdce66e1eea65c9bc509dfc1bc728cc
Mizanali Panjwani <mizanalip786@gmail.com>
2020-06-27 14:06:04 +0530

    Added copy button

---
+++ public/scripts/main.js
@@ -0,0 +1,21 @@
+function copy(){
+  var elm = document.querySelector(".bars");
+  // for Internet Explorer
+  if(document.body.createTextRange) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(elm);
+    range.select();
+    document.execCommand("Copy");
+    alert("Copied lyrics to clipboard!");
+  }
+  else if(window.getSelection) {
+    // other browsers
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(elm);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand("Copy");
+    alert("Copied lyrics to clipboard!");
+  }
+}
