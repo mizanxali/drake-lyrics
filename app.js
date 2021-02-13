032bd99741a6e97b1e4f5d024d5880975491b940
Mizanali Panjwani <mizanalip786@gmail.com>
2021-02-13 20:45:28 +0530

    Bug fix

--- app.js
+++ app.js
@@ -198,8 +198,8 @@ router.get("/api/lyrics", function(req, res){
 //serve static assets in production
 if(process.env.NODE_ENV === 'production') {
 	//set static folder
-	app.use(express.static('client/build'))
-	app.get('*', (req, res) => {
+	router.use(express.static('client/build'))
+	router.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 	})
 }
